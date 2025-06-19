0970cbeca46488efd9b1018eb883bd1088713c8d
anayatur1 <119777828+anayatur1@users.noreply.github.com>
2025-06-19 11:56:26 +0530

    Update IUserService.java

--- src/main/java/com/dailycodework/lakesidehotel/service/IUserService.java
+++ src/main/java/com/dailycodework/lakesidehotel/service/IUserService.java
@@ -4,10 +4,6 @@ import com.dailycodework.lakesidehotel.model.User;
 
 import java.util.List;
 
-/**
- * @author Simpson Alfred
- */
-
 public interface IUserService {
     User registerUser(User user);
     List<User> getUsers();
